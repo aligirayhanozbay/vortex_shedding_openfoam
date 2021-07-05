1976d2f2527d198069e983365431997afffc5a22
aligirayhanozbay <44897017+aligirayhanozbay@users.noreply.github.com>
2021-07-05 18:00:46 +0100

    gmsh format specified for newer gmsh versions

--- models/geometry_study/generate_mesh.sh
+++ models/geometry_study/generate_mesh.sh
@@ -4,7 +4,7 @@ for file in "$(pwd)"/*.geo
 do 
     echo "********************* start *****************************"
     echo "geo file:$file"
-    gmsh -3 $file -o ${file/.geo/.msh}
+    gmsh -3 $file -o ${file/.geo/.msh} -format msh2
     echo "msh file:${file/.geo/.msh}"
     echo "********************* end *****************************"
 done
